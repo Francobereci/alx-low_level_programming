fdcbdeb9c748ff16764836bee25d066c2ed7f447
Francobereci <okus1785@gmail.com>
2022-10-18 11:20:03 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,22 +2,22 @@
 
 /**
  * main - Print _putchar.
- *
+ * written by Vincent
  * Return: Always 0 (Success)
  */
 
 int main(void)
-{	
-	_putchar('_');
-     	_putchar('p');
-        _putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	{
-	_putchar('\n');
-	return (0);
-	}
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+{
+_putchar('\n');
+return (0);
+}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,6 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-
-int _putchar(char);
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -9,11 +8,10 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
- void jack_bauer(void);
+void jack_bauer(void);
 void times_table(void);
-int add(int a, int b);
+int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-void num(int r, int n);
 
 #endif
